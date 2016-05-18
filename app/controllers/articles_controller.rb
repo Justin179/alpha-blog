f7bbda62739ef06219f6be2ee4ce10b31b396825
Justin Chen <justin.uob@gmail.com>
2016-05-18 16:15:34 +0000

    fix create, add flash and error messages, create show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,12 +4,19 @@ class ArticlesController < ApplicationController
    end
 
    def create
-     #render plain: params[:article].inspect
-     @article = Article.new(article_params)
-     @article.save
-     redirect_to articles_show(@article)
+     @article = Article.new(article_params) # 接傳入的參數
+     if @article.save
+       flash[:notice] = "Article was successfully created"
+       redirect_to article_path(@article)
+     else # 檢核不過無法存檔時
+       render 'new'
+     end
    end
 
+   def show
+     @article = Article.find(params[:id])
+   end
+  
    private
    def article_params
      params.require(:article).permit(:title,:description)
