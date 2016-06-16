43d381699d71ec95da52772561701acee6509c4d
Lucas <wuchiwei17@gmail.com>
2016-06-16 06:03:34 +0000

    After update information, turn page to show information page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,21 @@
 class BooksController < ApplicationController
 
-def new 
-  @book = Book.new    
-end
+  def new 
+    @book = Book.new    
+  end
 
+  def show
+    @book = Book.find(params[:id])
+  end
+  
+  def create 
+    @book = Book.create(book_params)
+    flash[:success] = "Update Success!"
+    redirect_to @book
+  end
+
+  private
+    def book_params
+      params.require(:book).permit(:phone, :address, :opentime, :closetime)
+    end
 end
